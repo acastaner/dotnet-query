693a4cc6b07a263371c95ad46abbf46182a233e3
Arnaud <arnaud.castaner@outlook.com>
2025-04-09 16:09:16 +0200

    Add Full URL return

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,9 +7,17 @@ namespace DotNetQueryApp.Controllers
     {
         public IActionResult Index(string query)
         {
+            var request = Request;
+            string scheme = request.Scheme; // e.g., "http" or "https"
+            string host = request.Host.Value; // e.g., "localhost:5000"
+            string path = request.Path.Value; // e.g., "/demo/query/10"
+            string queryString = request.QueryString.Value; // e.g., "?param=value"
+            string fullUrl = $"{scheme}://{host}{path}{queryString}";
+
             var model = new QueryParameterModel
             {
-                QueryValue = query
+                QueryValue = query,
+                FullUrl = fullUrl
             };
             return View(model);
         }
--- Models/QueryParameterModel.cs
+++ Models/QueryParameterModel.cs
@@ -2,4 +2,8 @@ namespace DotNetQueryApp.Models;
 public class QueryParameterModel
 {
     public string QueryValue { get; set; }
+    public string FullUrl
+    {
+        get; set;
+    }
 }
